19442776916fb29d9f986c358eda548b9e345b6d
Devops-Engineer-24 <mayurdevops23@gmail.com>
2024-11-13 23:39:41 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,27 @@
+# variables.tf
+
+variable "aws_region" {
+  description = "The AWS region to deploy resources"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
+variable "subnet_cidr" {
+  description = "The CIDR block for the subnet"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "The AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The EC2 instance type"
+  type        = string
+  
+}
