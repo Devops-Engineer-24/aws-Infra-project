597c8165b93125d6a76d05cc8632979756b2d49e
Devops-Engineer-24 <mayurdevops23@gmail.com>
2024-11-13 18:15:03 +0530

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,11 +1,7 @@
-# modules/vpc/outputs.tf
-
 output "vpc_id" {
-  description = "The VPC ID"
-  value       = aws_vpc.main.id
+  value = aws_vpc.main.id
 }
 
 output "subnet_id" {
-  description = "The Subnet ID"
-  value       = aws_subnet.main.id
+  value = aws_subnet.main.id
 }
