9d2b02e04628bf80c2a346f31991e12337453bae
Devops-Engineer-24 <mayurdevops23@gmail.com>
2024-11-14 00:40:46 +0530

    Update outputs.tf

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,3 +1,5 @@
-output "ec2_public_ip" {  # Renaming output to avoid conflict
-  value = aws_instance.example.public_ip
+# modules/ec2/outputs.tf
+output "ec2_public_ip" {
+  description = "The public IP of the EC2 instance"
+  value = aws_instance.example[*].public_ip  # Access all instances' public IPs using the count.index
 }
