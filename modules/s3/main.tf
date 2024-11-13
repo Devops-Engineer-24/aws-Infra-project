9d7261a527b18c36b5ae296134457f722217b265
Devops-Engineer-24 <mayurdevops23@gmail.com>
2024-11-13 22:05:41 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,4 +1,3 @@
-# modules/s3/main.tf
 resource "aws_s3_bucket" "this" {
   bucket = var.bucket_name
   acl    = var.acl
@@ -8,12 +7,6 @@ resource "aws_s3_bucket" "this" {
   }
 
   tags = {
-    Name        = "Terraform-S3-Bucket"
     Environment = var.environment
   }
-}
-
-output "s3_bucket_name" {
-  value       = aws_s3_bucket.this.bucket
-  description = "The name of the S3 bucket"
 }
