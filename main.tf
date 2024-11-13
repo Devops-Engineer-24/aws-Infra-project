dda9d48b8e19c33b1b4237666552fd996c0e1275
Devops-Engineer-24 <mayurdevops23@gmail.com>
2024-11-13 18:08:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,23 @@
+# main.tf
+
+provider "aws" {
+  region = var.aws_region
+}
 
 module "vpc" {
-  source = "./modules/vpc"
+  source       = "./modules/vpc"
+  vpc_cidr     = var.vpc_cidr
+  subnet_cidr  = var.subnet_cidr
 }
+
 module "ec2" {
-  source = "./modules/ec2"
+  source      = "./modules/ec2"
+  ami_id      = var.ami_id
+  instance_type = var.instance_type
+  subnet_id   = module.vpc.subnet_id
+}
+
+output "ec2_public_ip" {
+  description = "The public IP of the EC2 instance"
+  value       = module.ec2.public_ip
 }
