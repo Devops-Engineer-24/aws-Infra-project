8b0c3a7d326729338ad8341cd96a91b4c136726f
Devops-Engineer-24 <mayurdevops23@gmail.com>
2024-11-13 22:28:46 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -10,7 +10,3 @@ output "subnet_id" {
   value       = module.vpc.subnet_id
 }
 
-output "s3_bucket_name" {
-  value       = aws_s3_bucket.this.bucket
-  description = "The name of the S3 bucket"
-}
