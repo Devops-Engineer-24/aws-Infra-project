20e9f3932156ed855e631176f5e822abbcdd9120
Devops-Engineer-24 <mayurdevops23@gmail.com>
2024-11-13 22:19:17 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -28,3 +28,24 @@ variable "instance_type" {
   type        = string
   default     = "t2.micro"
 }
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
+
+variable "acl" {
+  description = "The ACL of the S3 bucket (private, public-read, etc.)"
+  type        = string
+  default     = "private"
+}
+
+variable "versioning_enabled" {
+  description = "Enable versioning for the S3 bucket"
+  type        = bool
+  default     = true
+}
+
+variable "environment" {
+  description = "The environment tag for the S3 bucket (e.g., dev, prod)"
+  type        = string
+}
