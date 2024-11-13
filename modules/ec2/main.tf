0de960edeae294a80c56e2bbea79d28f59baf3d2
Devops-Engineer-24 <mayurdevops23@gmail.com>
2024-11-14 00:43:50 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -10,7 +10,13 @@ resource "aws_instance" "example" {
   }
 }
 
-output "public_ip" {
-  description = "The public IP of the EC2 instance"
-  value       = aws_instance.example.public_ip
+# modules/ec2/outputs.tf
+output "ec2_public_ip_first" {
+  description = "The public IP of the first EC2 instance"
+  value = aws_instance.example[0].public_ip  # First instance
+}
+
+output "ec2_public_ip_second" {
+  description = "The public IP of the second EC2 instance"
+  value = aws_instance.example[1].public_ip  # Second instance
 }
