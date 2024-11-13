4667e9dd18b008d0a23f492422729c8a79bd50e3
Devops-Engineer-24 <mayurdevops23@gmail.com>
2024-11-13 19:24:07 +0530

    Update variables.tf

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -1,11 +1,22 @@
-# modules/vpc/variables.tf
-
-variable "vpc_cidr" {
-  description = "The CIDR block for the VPC"
+# modules/s3/variables.tf
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
   type        = string
 }
 
-variable "subnet_cidr" {
-  description = "The CIDR block for the subnet"
+variable "acl" {
+  description = "The ACL of the S3 bucket (private, public-read, etc.)"
+  type        = string
+  default     = "private"
+}
+
+variable "versioning_enabled" {
+  description = "Enable versioning for the S3 bucket"
+  type        = bool
+  default     = true
+}
+
+variable "environment" {
+  description = "The environment tag for the S3 bucket (e.g., dev, prod)"
   type        = string
 }
