5c822d3ec231aca4e872b69c5c26149c12051c8b
Devops-Engineer-24 <mayurdevops23@gmail.com>
2024-11-13 22:07:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,7 +26,3 @@ output "ec2_public_ip" {
   value       = module.ec2.public_ip
 }
 
-output "s3_bucket_name" {
-  value       = module.s3.s3_bucket_name
-  description = "The name of the S3 bucket"
-}
