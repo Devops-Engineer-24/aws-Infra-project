79e0791adb8ff91894b6d03e8e3d33a18faa208b
Devops-Engineer-24 <mayurdevops23@gmail.com>
2024-11-14 00:38:29 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "example" {
   ami           = var.ami_id
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
+  count         = var.instance_count
   tags = {
     Name = "TerraformInstance"
   }
