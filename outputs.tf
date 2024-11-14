fc1a49cc90a31c0a841b9761d1b04fb5f8cbd69e
Devops-Engineer-24 <mayurdevops23@gmail.com>
2024-11-14 10:50:57 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -9,9 +9,3 @@ output "subnet_id" {
   description = "The Subnet ID"
   value       = module.vpc.subnet_id
 }
-
-# main.tf
-output "ec2_public_ips" {
-  description = "Public IPs of EC2 instances from the ec2 module"
-  value = module.ec2.ec2_public_ips  # Access the output defined in the ec2 module
-}
